05c9a450731626d85c2aaf3291bdb049969d257e
gabrielfxz <gabrielperozim7@gmail.com>
2023-06-26 17:39:21 -0300

    Projeto Finalizado

---
+++ ScreenSound/Models/Avaliacao.cs
@@ -0,0 +1,23 @@
+﻿namespace ScreenSound.Models;
+
+
+internal class Avaliacao
+{
+
+    public Avaliacao(int nota)
+    {
+        if (nota < 0 ) nota = 0;
+        if (nota > 10) nota = 10;
+        Nota = nota;
+    } 
+
+    public int Nota { get; }
+
+    public static Avaliacao Parse(string s)
+    {
+        int nota = int.Parse(s);
+        return new Avaliacao(nota);
+    }
+
+
+}
